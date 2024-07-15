dd4172f069e95218118db4a2d8788a4073ce3b83
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-07-15 21:03:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'N
 const ul = document.getElementById('bands');
 
 function strip(bandName) {
-	return bandName.replace(/^(a |the |an)/i, '').trim();
+	return bandName.replace(/^(a |the |an )/i, '').trim();
 }
 
-const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1);
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
 ul.innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
